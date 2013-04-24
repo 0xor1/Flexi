c2ba25b6b7ce4d52e930c3091f1827925dd7358e
Daniel Robinson <daniel.robinson.open@gmail.com>
2013-04-24 22:13:22 +0100

    added domInfo object for Region.

--- src/Eventable/Dom/Region.js
+++ src/Eventable/Dom/Region.js
@@ -5,39 +5,44 @@
 
 (function(NS){
 
+    /**
+     * Regions exists purely to hold content and to allow splitting into BiRegions
+     */
     var ns = window[NS] = window[NS] || {}
-        , rs = ns.rs;
+        , rs = ns.rs
+        , domInfo = {
+            tag: 'div',
+            class: NS + "-region",
+            style: {
+                position: 'relative',
+                height: '100%',
+                width: '100%',
+                margin: 0,
+                padding: 0,
+                border: 0,
+                overflow: 'hidden',
+                display: 'block'
+            }
+        };
 
     ns.Region = function(){
 
-        ns.Dom.call(this, {
-            tag: rs.d,
-            style: {
-                position: 'relative'
-            },
-            children: [
-            ]
-        });
+        ns.Dom.call(this, domInfo);
 
     };
 
-
-    ns.Region.htmlGenerator = function(){
-
-    };
 
     ns.Region.prototype = Object.create(ns.Dom.prototype);
 
 
-    ns.Region.prototype.addChild = function(){
+    ns.Region.prototype.addChild = function(child, position, orientation){
 
     };
 
 
-    ns.Region.prototype.removeChild = function(){
+    ns.Region.prototype.removeChild = function(child){
 
     };
-
 
 
 })(NS);
